199b4e69dc77b9d5a8988faa4fa91974bee20ed1
Donovan Harrison <donjuan0802@gmail.com>
2025-03-16 00:29:18 -0500

    Feat: Created Expense Model

---
+++ src/main/java/io/github/dhar135/expense_tracker/model/Expense.java
@@ -0,0 +1,23 @@
+package io.github.dhar135.expense_tracker.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private BigDecimal amount;
+    private LocalDate date;
+    private ExpenseCategory category;
+}
---
+++ src/main/java/io/github/dhar135/expense_tracker/model/ExpenseCategory.java
@@ -0,0 +1,12 @@
+package io.github.dhar135.expense_tracker.model;
+
+public enum ExpenseCategory {
+    GROCERIES,
+    UTILITIES,
+    RENT,
+    ENTERTAINMENT,
+    TRANSPORTATION,
+    HEALTH,
+    INSURANCE,
+    OTHER
+}
